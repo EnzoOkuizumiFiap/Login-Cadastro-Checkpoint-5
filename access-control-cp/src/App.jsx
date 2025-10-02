2f9c32031e96c58a6fb215087466d0c62356d901
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2025-10-02 16:16:35 -0300

    feat: Colocando Outlet no App.jsx

--- access-control-cp/src/App.jsx
+++ access-control-cp/src/App.jsx
@@ -1,11 +1,13 @@
 import Cabecalho from "./components/Cabecalho/cabecalho";
+import { Outlet } from "react-router-dom";
+import './globals.css';
 
 export default function App() {
 
   return (
     <div className="container">
       <Cabecalho />
-      <p>Teste</p>
+      <Outlet />
     </div>
   );
 }
