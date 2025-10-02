15fc2e8f7e57f2015a1435b2199e0f9e8f177357
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2025-10-02 16:15:29 -0300

    feat: Criando e arrumando as rotas de Cadastro e login

--- access-control-cp/src/main.jsx
+++ access-control-cp/src/main.jsx
@@ -5,10 +5,15 @@ import './globals.css'
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './routes/Home/index.tsx';
+import Cadastro from './routes/Cadastro/index.tsx'
+import Login from './routes/Login/index.tsx'
+import Error from './routes/Error/index.tsx'
 
 const router = createBrowserRouter([
   {path:"/", element: <App/>, errorElement:<Error/>, children:[
     {path:"/", element:<Home/>},
+    {path:"cadastro", element:<Cadastro/>},
+    {path:"login", element:<Login />},
   ]}
 ])
 
