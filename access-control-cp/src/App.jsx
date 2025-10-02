c7fdb9620700dc932b70701966df6af8414c73d3
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2025-10-02 15:15:04 -0300

    feat: Criando Cabecalho e Menu

--- access-control-cp/src/App.jsx
+++ access-control-cp/src/App.jsx
@@ -1,7 +1,10 @@
+import Cabecalho from "./components/Cabecalho/cabecalho";
+
 export default function App() {
 
   return (
     <div className="container">
+      <Cabecalho />
       <p>Teste</p>
     </div>
   );
