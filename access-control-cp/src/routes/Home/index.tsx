74e47abacd5991c05e3bb03ce208c66eec267a92
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2025-10-02 16:24:38 -0300

    feat: adicionando dot env e um comentário Home

--- access-control-cp/src/routes/Home/index.tsx
+++ access-control-cp/src/routes/Home/index.tsx
@@ -2,6 +2,7 @@ export default function Home(){
     return(
         <main>
             <h1>Home</h1>
+            {/*Provavelmente aqui vamos exibir o Nome do usuário / Email*/}
         </main>
     );
 }
