52feb17f21c40b6c3f143afeecd429884d750f64
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2025-10-04 17:49:37 -0300

    bugFix: Arrumando a página a página de Cadastro e adicionando alguns arquivos de configuração

--- access-control-cp/src/routes/Cadastro/index.tsx
+++ access-control-cp/src/routes/Cadastro/index.tsx
@@ -1,7 +1,7 @@
 import { useForm } from "react-hook-form";
 import type { TipoUser } from "../../types/tipoUser";
 import { useNavigate } from "react-router-dom";
-const API_URL = import.meta.env.VITE_API_URL_USUARIOS as string;
+const API_URL = import.meta.env.VITE_API_URL_USUARIOS;
 
 export default function Cadastro() {
 
@@ -29,27 +29,27 @@ export default function Cadastro() {
         <main>
             <h1>Página de Cadastro</h1>
 
-            <div className="mx-auto w-[40%]">
+            <div className="mx-auto ">
                 <form onSubmit={onSubmit} className="frmCadastro">
                     <fieldset>
                         <legend>Se Cadastre no sistema</legend>
                         <div>
                             <label htmlFor="idNome">Nome:</label>
-                            <input type="text" id="idNome" className="bg-amber-200" {...register("nome", { required: true ,maxLength: 200})} aria-invalid={!!errors.nome} aria-describedby={errors.nome ? "nome-error" : undefined} /> {errors.nome && <span role="alert" id="nome-error" className="text-red-600 bg-red-300 border-[1px] border-red-600 rounded-md p-2">{errors.nome.message}</span>}
+                            <input type="text" id="idNome" className="bg-amber-200" {...register("nome", { required: "O nome é obrigatório",  minLength: { value: 3, message: "O nome deve ter no mínimo 3 caracteres" } })} aria-invalid={!!errors.nome} aria-describedby={errors.nome ? "nome-error" : undefined} /> {errors.nome && <span role="alert" id="nome-error" className="text-red-600 bg-red-300 border-[1px] border-red-600 rounded-md p-2">{errors.nome.message}</span>}
                         </div>
 
                         <div>
                             <label htmlFor="idNomeUsuario">Nome Usuário:</label>
-                            <input type="text" id="idNomeUsuario" className="bg-amber-200" {...register("nome", { required: true ,maxLength: 200})} aria-invalid={!!errors.nome} aria-describedby={errors.nome ? "nome-error" : undefined} /> {errors.nome && <span role="alert" id="nome-error" className="text-red-600 bg-red-300 border-[1px] border-red-600 rounded-md p-2">{errors.nome.message}</span>}
+                            <input type="text" id="idNomeUsuario" className="bg-amber-200" {...register("nomeUsuario", { required: "O nome de usuário é obrigatório", minLength: { value: 3, message: "O nome de usuário deve ter no mínimo 3 caracteres" } })} aria-invalid={!!errors.nomeUsuario} aria-describedby={errors.nomeUsuario ? "nomeUsuario-error" : undefined} /> {errors.nomeUsuario && <span role="alert" id="nomeUsuario-error" className="text-red-600 bg-red-300 border-[1px] border-red-600 rounded-md p-2">{errors.nomeUsuario.message}</span>}
                         </div>
 
                         <div>
                             <label htmlFor="idEmail">Email:</label>
-                            <input type="email" id="idEmail" className="bg-amber-200" {...register("email", { required: true ,maxLength: 200})} aria-invalid={!!errors.email} aria-describedby={errors.email ? "email-error" : undefined} /> {errors.email && <span role="alert" id="email-error" className="text-red-600 bg-red-300 border-[1px] border-red-600 rounded-md p-2">{errors.email.message}</span>}
+                            <input type="email" id="idEmail" className="bg-amber-200" {...register("email", { required: "O email é obrigatório", pattern: { value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, message: "Formato de email inválido" } })} aria-invalid={!!errors.email} aria-describedby={errors.email ? "email-error" : undefined} /> {errors.email && <span role="alert" id="email-error" className="text-red-600 bg-red-300 border-[1px] border-red-600 rounded-md p-2">{errors.email.message}</span>}
                         </div>
                         
                         <div>
-                        <button type="submit">Entrar</button>
+                        <button type="submit">Cadastrar</button>
                         </div>
                     </fieldset>
                 </form>
--- access-control-cp/src/vite-env.d.ts
+++ access-control-cp/src/vite-env.d.ts
@@ -1,9 +1,1 @@
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_API_URL_USUARIOS: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
