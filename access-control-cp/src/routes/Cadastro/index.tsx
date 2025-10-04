7a026ecd0ed32ac2776d331822df66a4a0b4eb97
LuzBGouveia <lu.barros.gouveia2@gmail.com>
2025-10-04 16:37:50 -0300

    Criando o template dos formulários das rotas de cadastro e login com seus campos e labels

--- access-control-cp/src/routes/Cadastro/index.tsx
+++ access-control-cp/src/routes/Cadastro/index.tsx
@@ -1,5 +1,33 @@
 export default function Cadastro() {
-    return(
-        <h1>Página de Cadastro</h1>
-    );
+  return (
+    <main>
+      <h1>Criar uma conta</h1>
+      <div className="mx-auto w-[40%]">
+        <form className="cadastrarUser">
+          <fieldset>
+            <legend>Informações</legend>
+            <div>
+              <label htmlFor="idNome">Nome de usuário:</label>
+              <input type="text" id="idNome" className="bg-amber-50" />
+            </div>
+            <div>
+              <label htmlFor="idEmail">Email:</label>
+              <input type="text" id="idEmail" className="bg-amber-50" />
+            </div>
+            <div>
+              <label htmlFor="idSenha">Senha:</label>
+              <input type="text" id="idSenha" className="bg-amber-50" />
+            </div>
+            <div>
+              <label htmlFor="idSenha2">Repita a senha:</label>
+              <input type="text" id="idSenha2" className="bg-amber-50" />
+            </div>
+            <div>
+              <button type="submit">Criar conta</button>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+    </main>
+  );
 }
--- access-control-cp/src/routes/Login/index.tsx
+++ access-control-cp/src/routes/Login/index.tsx
@@ -1,8 +1,29 @@
 export default function Login() {
-    return(
-        <main>
-            <h1>Página de Login</h1>
-            {/*Provavelmente aqui vamos exibir o Nome do usuário / Email*/}            
-        </main>
-    );
+  return (
+    <main>
+      <h1>Entrar em sua conta</h1>
+      <div className="mx-auto w-[40%]">
+        <form className="logarUser">
+          <fieldset>
+            <legend>Informações</legend>
+            <div>
+              <label htmlFor="idNome">Nome de usuário:</label>
+              <input type="text" id="idNome" className="bg-amber-50" />
+            </div>
+            <div>
+              <label htmlFor="idEmail">Email:</label>
+              <input type="text" id="idEmail" className="bg-amber-50" />
+            </div>
+            <div>
+              <label htmlFor="idSenha">Senha:</label>
+              <input type="text" id="idSenha" className="bg-amber-50" />
+            </div>
+            <div>
+              <button type="submit">Entrar</button>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+    </main>
+  );
 }
