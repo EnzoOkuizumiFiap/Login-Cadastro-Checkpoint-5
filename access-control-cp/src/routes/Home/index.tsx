dbe8842fc034020aec65306ed8b8ca84653647c8
milton <smarcelino.mj@gmail.com>
2025-10-05 13:08:00 -0300

    Feat: Estilizando o h1 do home

--- access-control-cp/src/routes/Home/index.tsx
+++ access-control-cp/src/routes/Home/index.tsx
@@ -3,7 +3,7 @@ export default function Home() {
 
     return (
         <main>
-            <h1>Home</h1>
+            <h1 className="text-center font-bold text-3xl mb-5">Home</h1>
 
             <div className="logado">
                 {usuarioLogado ? (
