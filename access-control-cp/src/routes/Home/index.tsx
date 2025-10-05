707b69c47a0ae4d284f2f77792923e776f99d928
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2025-10-04 23:14:10 -0300

    feat: Adicionando informações do usuário quando logado na página principal

--- access-control-cp/src/routes/Home/index.tsx
+++ access-control-cp/src/routes/Home/index.tsx
@@ -1,8 +1,13 @@
 export default function Home(){
+
+    const usuarioLogado = JSON.parse(localStorage.getItem("usuarioLogado") || "{}");
+
     return(
         <main>
             <h1>Home</h1>
             {/* Aqui vamos exibir o nome do Usuário e email */}
+
+            <p>Bem-vindo, {usuarioLogado.nomeUsuario} com email: {usuarioLogado.email}</p>
         </main>
     );
 }
