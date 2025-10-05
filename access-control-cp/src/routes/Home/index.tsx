c99bb66c7e496ae2498b5f1945712de641ac3ff9
milton <smarcelino.mj@gmail.com>
2025-10-05 13:31:10 -0300

    feat: apagando o código do usuário logado na pagina home.

--- access-control-cp/src/routes/Home/index.tsx
+++ access-control-cp/src/routes/Home/index.tsx
@@ -1,17 +1,10 @@
-export default function Home() {
-    const usuarioLogado = JSON.parse(localStorage.getItem("usuarioLogado") || "null");
+export default function Home(){
 
     return (
         <main>
             <h1 className="text-center font-bold text-3xl mb-5">Home</h1>
 
-            <div className="logado">
-                {usuarioLogado ? (
-                    <p>Bem-vindo, {usuarioLogado.nomeUsuario} com email: {usuarioLogado.email}</p>
-                ) : (
-                    <p>Você não está logado.</p>
-                )}
-            </div>
+        
         </main>
     );
 }
