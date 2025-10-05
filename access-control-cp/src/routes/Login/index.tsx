1f4abd78b09e434b7a382c2071770441c746fac4
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2025-10-04 23:13:41 -0300

    feat: Lógica da página de Login

--- access-control-cp/src/routes/Login/index.tsx
+++ access-control-cp/src/routes/Login/index.tsx
@@ -14,9 +14,29 @@ export default function Login() {
 
     const onSubmit = handleSubmit(async (data:TipoUser) => {
 
-        {/*Agora falta adicionar a lógica do login... */}
+        try {
+            const response = await fetch(API_URL);
+            if(!response.ok) throw new Error("Erro ao buscar usuários");
 
-        navigate("/home");
+            const usuarios: TipoUser[] = await response.json();
+
+            const usuarioValido = usuarios.find(
+                (user) => user.nomeUsuario.toLowerCase() === data.nomeUsuario.toLowerCase() && user.email.toLowerCase() === data.email.toLowerCase()
+            );
+
+            if(usuarioValido) {
+                localStorage.setItem("usuarioLogado", JSON.stringify(usuarioValido))
+                navigate("/")
+            } else {
+                alert("Usuário ou E-mail Inválido.")
+            }
+
+        } catch (error) {
+            console.error("Erro no Login:", error)
+            alert("Erro ao logar. Tente novamente.");
+        }
+
+        navigate("/");
     });
 
 
@@ -30,15 +50,15 @@ export default function Login() {
                         <legend>Entrar no sistema</legend>
                         <div>
                             <label htmlFor="idNomeUsuario">Nome Usuário:</label>
-                            <input type="text" id="idNomeUsuario" className="bg-amber-200" {...register("nome", { required: true ,maxLength: 200})} aria-invalid={!!errors.nome} aria-describedby={errors.nome ? "nome-error" : undefined} /> {errors.nome && <span role="alert" id="nome-error" className="text-red-600 bg-red-300 border-[1px] border-red-600 rounded-md p-2">{errors.nome.message}</span>}
+                            <input type="text" id="idNomeUsuario" className="bg-amber-200" {...register("nomeUsuario", { required:"Insira Nome do Usuário" })} aria-invalid={!!errors.nome} aria-describedby={errors.nome ? "nome-error" : undefined} /> {errors.nome && <span role="alert" id="nome-error" className="text-red-600 bg-red-300 border-[1px] border-red-600 rounded-md p-2">{errors.nome.message}</span>}
                         </div>
                         <div>
                             <label htmlFor="idEmail">Email:</label>
-                            <input type="email" id="idEmail" className="bg-amber-200" {...register("email", { required: true ,maxLength: 200})} aria-invalid={!!errors.email} aria-describedby={errors.email ? "email-error" : undefined} /> {errors.email && <span role="alert" id="email-error" className="text-red-600 bg-red-300 border-[1px] border-red-600 rounded-md p-2">{errors.email.message}</span>}
+                            <input type="email" id="idEmail" className="bg-amber-200" {...register("email", { required:"Insira Email do usuário" })} aria-invalid={!!errors.email} aria-describedby={errors.email ? "email-error" : undefined} /> {errors.email && <span role="alert" id="email-error" className="text-red-600 bg-red-300 border-[1px] border-red-600 rounded-md p-2">{errors.email.message}</span>}
                         </div>
                         
                         <div>
-                        <button type="submit">Entrar</button>
+                            <button type="submit">Entrar</button>
                         </div>
 
                         <div>
