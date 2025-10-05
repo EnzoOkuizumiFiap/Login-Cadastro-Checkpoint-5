8729934dd0279eaa29bfa72c95308f44adb07cd2
lucastdag <lucastav000@gmail.com>
2025-10-05 14:47:21 -0300

    feat: adicionado link redirecionando para Home se URL estiver errado.

--- access-control-cp/src/routes/Error/index.tsx
+++ access-control-cp/src/routes/Error/index.tsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function Error(){
     return(
         <main>
             <h1>Not Found - Erro 404</h1>
+            <Link to="/Home"> Voltar para a Página Inicial</Link>
         </main>
     );
 }
