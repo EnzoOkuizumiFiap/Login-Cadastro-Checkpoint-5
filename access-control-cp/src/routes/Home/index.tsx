f072f1a7e21e9c48a218604749ec1af6383a56cf
milton <smarcelino.mj@gmail.com>
2025-10-05 13:36:28 -0300

    feat: arrumando usuário logado na pagina Home

--- access-control-cp/src/routes/Home/index.tsx
+++ access-control-cp/src/routes/Home/index.tsx
@@ -1,10 +1,12 @@
+import Logado from "../../components/Logado/logado";
+
 export default function Home(){
 
     return (
         <main>
             <h1 className="text-center font-bold text-3xl mb-5">Home</h1>
 
-        
-        </main>
-    );
+            <Logado/>
+        </main> 
+    ); 
 }
